6db89195b2c1d756e75dee8c654c7e6a933d5b8a
crischlr <cristinachlr@gmail.com>
2017-01-26 11:26:38 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate the inverse of a matrix x wich is supposed not singular.
+## Once it is calculated the first time, it is cached so that it can be used again
+## if needed (for example, in a loop).
+## This is saving time, in case you need to use the inverse of the matrix x repeteadly,
+## as the calculation is a very time consuming operation.
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix function creates a vector containing functions which will be used
+##   - set(): Set values to cached matrices 'x' and 'inv'
+##   - get(): Returns the cached matrix 'x'
+##   - setinv(): Set the value to cached matrix 'inv'   
+##   - getinv(): Returns the value of cached matrix 'inv'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    
+    setinv <- function(inverse) inv <<- inverse
+    
+    getinv <- function() inv
+    
+    list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function returns a matrix that is the inverse of 'x'
+## First, it tries to get the cached inverse matrix if exists.
+## If the inverse matrix has not been cached yet it is calculated and cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  inv <- x$getinv()
+  
+  if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+  }
+  
+  data <- x$get()
+  
+  inverse <<- solve(data)
+  
+  x$setinv(inverse)
+  
+  inverse
 }
